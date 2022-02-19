39f73652aae5a6457d9115f60e32f3c008a3c55e
granitsa-online <granitsa-online@inbox.ru>
2022-02-19 03:07:56 +0300

    Ответ на кейс 3.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_calculator/screens/files_demo_screen.dart';
 import 'package:pizza_calculator/screens/list_view_screen.dart';
 import 'package:pizza_calculator/screens/shared_pref_screen.dart';
 
 void main() {
-  runApp(const SharedPrefScreen());
+  runApp(const FilesDemoScreen ());
 }
